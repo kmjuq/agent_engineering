f73a7183ac684737302bd42e5da9336bb3abe522
k22048 <k22048@nbcb.com>
2026-05-19 20:54:58 +0800

    学习langgraph

--- utils/std_model.py
+++ utils/std_model.py
@@ -15,14 +15,14 @@ def model_template(model_name):
 
 @auto_load_env
 def base_model():
-    return model_template(os.getenv("ALIYUN_TEXT_MODEL"))
+    return model_template("deepseek-v4-flash")
 
 
 @auto_load_env
 def basic_model():
-    return model_template("qwen-plus")
+    return model_template("qwen3.5-27b")
 
 
 @auto_load_env
 def advanced_model():
-    return model_template("qwen-turbo")
+    return model_template("qwen3.6-plus")
