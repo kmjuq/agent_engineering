8f5ba9a965dd94f8f74ca3dda051230643f95241
k22048 <k22048@nbcb.com>
2026-06-05 17:30:57 +0800

    学习hitl

--- utils/std_model.py
+++ utils/std_model.py
@@ -1,14 +1,10 @@
-import os
-
-from langchain_openai import ChatOpenAI
+from langchain_deepseek import ChatDeepSeek
 
 from utils.env import auto_load_env
 
 
 def model_template(model_name):
-    return ChatOpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"),
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    return ChatDeepSeek(
         model=model_name,
     )
 
