8f02961ae2e62d2c729475fbb48490be6ae125fd
k22048 <k22048@nbcb.com>
2026-06-26 17:27:34 +0800

    hello-agents

--- utils/env.py
+++ utils/env.py
@@ -11,6 +11,9 @@ from dotenv import load_dotenv
 _PROJECT_ROOT = Path(__file__).resolve().parent.parent
 _ENV_PATH = _PROJECT_ROOT / ".env"
 
+def load_env():
+    load_dotenv(dotenv_path=_ENV_PATH)
+
 
 def auto_load_env(func):
 
